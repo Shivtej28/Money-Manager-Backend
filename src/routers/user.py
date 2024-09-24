5200e452e5a19a42a4dbf8edc71ec0aae64e3897
Shivtej Deshmukh <shivtejhd@gmail.com>
2024-09-24 22:57:44 +0530

    Updated user.py

--- src/routers/user.py
+++ src/routers/user.py
@@ -18,7 +18,7 @@ def create_user(user : UserBase, db : Session =  Depends(get_db)):
     return JSONResponse(status_code = response.status_code, content = response.to_dict())
 
 @router.get("/user/login")
-def login_user(user: UserLogin = Query() , db: Session = Depends(get_db)):
+def login_user(user: UserLogin = Depends() , db: Session = Depends(get_db)):
     user_to_login = user.dict()
     print(user_to_login)    
     response = user_service.login_user(db, user_to_login)
